50e30ff2032e6c2c4c494b8e246567b4f71139c1
kcaBmask <37028017+kcaBmask@users.noreply.github.com>
2024-07-23 01:30:58 +0800

    Update f2b.sh

--- f2b.sh
+++ f2b.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Define ANSI escape codes for bold, colors, and no color
+RED='\033[1;31m'
+BOLD='\033[1;32m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Print green header
+echo -e "${GREEN}#########################################################################${NC}"
+echo -e "${BOLD}                 
+echo -e "${BOLD}"                 AdoPisoft fail2ban install script${NC}"
+
+# Bash ASCII logo with green text and no background color
+echo -e "${GREEN}
+        __                ___.                          __    
+       |  | __ ____ _____ \\_ |__   _____ _____    _____|  | __
+       |  |/ // ___\\__  \\ | __ \\ /     \\__  \\  /  ___/  |/ /
+       |    <\\  \\___ / __ \\| \\_\\ \\  Y Y  \\/ __ \\_\\___ \\|    < 
+       |__|_ \\___  >____  /___  /__|_|  (____  /____  >__|_ \\
+           \\/    \\/     \\/    \\/      \\/     \\/     \\/     \\/
+${NC}"
+
+# Print green header
+echo -e "${GREEN}#########################################################################${NC}"
+
 # Variables
 FILTER_FILE="/etc/fail2ban/filter.d/adopisoft.conf"
 JAIL_FILE="/etc/fail2ban/jail.local"
@@ -15,13 +39,13 @@ check_status() {
 }
 
 # Install Fail2ban
-echo "Installing Fail2ban..."
-sudo apt-get update
-sudo apt-get install -y fail2ban
+echo -e "${BOLD}Installing fail2ban...${NC}"
+sudo apt-get update  >/dev/null 2>&1
+sudo apt-get install -y fail2ban  >/dev/null 2>&1
 check_status
 
 # Create or update the filter configuration
-echo "Creating/updating Fail2ban filter configuration..."
+echo -e "${BOLD}Creating/updating Fail2ban filter configuration...${NC}"
 sudo bash -c "cat > $FILTER_FILE <<EOF
 [Definition]
 failregex = \[\[ Failed%%20login%%20attempt%%3A%%20username%%3D\w+%%2C%%20ip%%3D<HOST> \]\]
@@ -30,7 +54,7 @@ EOF"
 check_status
 
 # Create or update the jail configuration
-echo "Creating/updating Fail2ban jail configuration..."
+echo -e "${BOLD}Creating/updating Fail2ban jail configuration...${NC}"
 sudo bash -c "cat > $JAIL_FILE <<EOF
 [adopisoft]
 enabled = true
@@ -49,20 +73,20 @@ EOF"
 check_status
 
 # Enable Fail2ban to start on boot
-echo "Enabling Fail2ban to start on boot..."
-sudo systemctl enable fail2ban
+echo -e "${BOLD}Enabling Fail2ban to start on boot...${NC}"
+sudo systemctl enable fail2ban  >/dev/null 2>&1
 check_status
 
 # Restart Fail2ban
-echo "Restarting Fail2ban..."
-sudo systemctl restart fail2ban
+echo -e "${BOLD}Restarting Fail2ban...${NC}"
+sudo systemctl restart fail2ban  >/dev/null 2>&1
 check_status
 sleep 5
 
 # Check Fail2ban status
-echo "Checking Fail2ban status..."
+echo -e "${BOLD}Checking Fail2ban status...${NC}"
 sudo fail2ban-client status adopisoft
 sudo fail2ban-client status sshd
 check_status
 
-echo "Fail2ban setup completed successfully."
+echo -e "${BOLD}Fail2ban setup completed successfully.${NC}"
