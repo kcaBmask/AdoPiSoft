0e7450f1fb25bc5e25029702ee33480e652ad92f
kcaBmask <37028017+kcaBmask@users.noreply.github.com>
2024-11-10 17:35:00 +0800

    Update ado1804.sh

--- ado1804.sh
+++ ado1804.sh
@@ -177,15 +177,14 @@ if [ "$os_version" == "18.04" ]; then
         exit 1
     fi
 
-    # Download and install AdoPiSoft
-print_bold "Downloading and installing AdoPiSoft..."
-if sudo wget -O /tmp/adopisoft-5.1.5-amd64-node-v16.4.0.deb https://github.com/AdoPiSoft/Releases/releases/download/v5.1.5/adopisoft-5.1.5-amd64-node-v16.4.0.deb >/dev/null 2>&1 && sudo apt install -y /tmp/adopisoft-5.1.5-amd64-node-v16.4.0.deb >/dev/null 2>&1; then
-    print_bold "AdoPiSoft installed successfully."
+   # Download the AdoPiSoft .deb file
+if wget -O /tmp/adopisoft-5.1.5-amd64-node-v16.4.0.deb https://github.com/AdoPiSoft/Releases/releases/download/v5.1.5/adopisoft-5.1.5-amd64-node-v16.4.0.deb; then
+    # Install the downloaded .deb file
+    sudo apt install /tmp/adopisoft-5.1.5-amd64-node-v16.4.0.deb
+    echo "Installation of AdoPiSoft completed successfully."
 else
-    print_error "Failed to install AdoPiSoft."
-    exit 1
+    echo "Download failed. Please check the URL or network connection."
 fi
-
 
     # Install PostgreSQL
     print_bold "Installing PostgreSQL..."
